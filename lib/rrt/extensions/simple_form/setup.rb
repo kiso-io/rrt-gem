cb4c4bc40b486e09340dd26b806545b3d3319368
John McDowall <john@mcdowall.info>
2019-08-19 00:39:19 -0700

    Simple form CSS class fixup

--- lib/rrt/extensions/simple_form/setup.rb
+++ lib/rrt/extensions/simple_form/setup.rb
@@ -111,7 +111,7 @@ SimpleForm.setup do |config|
   config.wrappers :horizontal_radio_and_checkboxes, **horizontal_options do |b|
     basic_input.call(b, :boolean)
     b.use :label, class: horizontal_label_class
-    b.wrapper class: "#{horizontal_right_class} cusom-control custom-checkbox" do |ba|
+    b.wrapper class: "#{horizontal_right_class} custom-control custom-checkbox" do |ba|
       ba.use :input, **input_options, class: 'form-check-input'
       error_and_hint.call(ba)
     end
